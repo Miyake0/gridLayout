fb7681c58e45fe92adf82f57c0a3532a18c645db
Ruslan <axelezgames@gmail.com>
2023-07-02 19:46:42 +0500

    tailwindcss test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./style/input.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1 className="">hi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../style/header.css";
+
+function Header() {
+  return (
+    <div>
+      {/* <div className="headerTitle">PORTFOLIO</div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <h3 class="animate-charcter"> EKOSH</h3>
+            </div>
+          </div>
+        </div> */}
+      <div className=" bg-red-300">
+        <div className="p-10 text-center">bonj</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
